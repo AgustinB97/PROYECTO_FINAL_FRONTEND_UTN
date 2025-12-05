e072b9ebaa7e6ed0f81c2a3a1c1402eaf49dd471
Agustinbaigo <agustinbaigorria97@gmail.com~>
2025-12-05 11:46:36 -0300

    createchats fix

--- src/Context/SocketContext.jsx
+++ src/Context/SocketContext.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import ENVIRONMENT from "../config/enviroment";
-import { createContext, useEffect, useRef,useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 
 export const SocketContext = createContext(null);
 
@@ -12,7 +12,10 @@ export const SocketProvider = ({ children, userId }) => {
         if (!userId) return;
 
         const newSocket = io(ENVIRONMENT.URL_API, { withCredentials: true });
-        socketRef(newSocket)
+
+        socketRef.current = newSocket;
+        
+        setSocket(newSocket);
 
         newSocket.emit("register_user", userId);
 
